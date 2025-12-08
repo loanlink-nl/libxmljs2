63a5b7af02799ff5741d38ec6a699b83450e9df5
Max Stevens <maxstevens2708@gmail.com>
2025-12-08 18:01:42 +0100

    fix build

--- scripts/configure.ts
+++ scripts/configure.ts
@@ -5,5 +5,8 @@ const platform = process.platform; // 'darwin', 'linux', 'win32'
 if (platform === "darwin" || platform === "linux") {
   await $`cd vendor/libxml2 && ./autogen.sh --with-schematron && cd ../..`;
 } else if (platform === "win32") {
-  await $`cd vendor/libxml2 && cmake . && cd ../..`;
+  const cmakeArgs = process.env.CMAKE_TOOLCHAIN_FILE
+    ? `-DCMAKE_TOOLCHAIN_FILE="${process.env.CMAKE_TOOLCHAIN_FILE}"`
+    : "";
+  await $`cd vendor/libxml2 && cmake . ${cmakeArgs} && cd ../..`;
 } 
