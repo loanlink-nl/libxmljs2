fda68757591fc1b0a4167e42055fb88e6a491dd9
Max Stevens <maxstevens2708@gmail.com>
2025-12-08 17:21:32 +0100

    fix

--- scripts/configure.ts
+++ scripts/configure.ts
@@ -5,5 +5,5 @@ const platform = process.platform; // 'darwin', 'linux', 'win32'
 if (platform === "darwin" || platform === "linux") {
   await $`cd vendor/libxml2 && ./autogen.sh --with-schematron && cd ../..`;
 } else if (platform === "win32") {
-  await $`cd vendor/libxml2 && cmake . --with-schematron && cd ../..`;
+  await $`cd vendor/libxml2 && cmake . && cd ../..`;
 } 
